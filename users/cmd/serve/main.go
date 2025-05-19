7e4b3cae70609a6b5ba05fb828088256ef80175e
Tom W. Yedwab <sincerelyyourstom@gmail.com>
2025-05-18 21:28:23 -0700

    Bump version to 1.0.3

--- users/cmd/serve/main.go
+++ users/cmd/serve/main.go
@@ -16,7 +16,7 @@ import (
 	"github.com/tomyedwab/yesterday/users/state"
 )
 
-const Version = "1.0.2"
+const Version = "1.0.3"
 
 type ChangePasswordRequest struct {
 	Username string `json:"username"`
